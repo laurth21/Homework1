0639dd44957280880fee0c67688f936d7d87066d
laurth21 <e11779607@student.tuwien.ac.at>
2024-03-19 18:11:23 +0100

    Add if statement for dividing by zero

--- functions.py
+++ functions.py
@@ -4,19 +4,29 @@ def serialResistor(R1,R2):
 
 def parallelResistor(R1,R2):
     """Calculating the overall resistor value of two resistors R1 and R2 in parallel by multiplying their values and dividing them by their sum"""
-    return (R1*R2/(R1+R2))
+    if R1 == 0 and R2 == 0:
+        return "Can't divide by zero"
+    else:
+        return (R1*R2/(R1+R2))
 
 def current(R,U):
     """Calculating the current throw a resistor by using the Ohm's law"""
-    return U/R
+    if R == 0:
+        return "Can't divide by zero"
+    else:
+        return U/R
 
 def DcPower(R,UI,UorI):
     """Calculating the power by a resistor using the voltage or current throw the resistor"""
     if UorI == 0:
-        """0 for using voltage U"""
-        return UI**2/R
+        # 0 for using voltage U
+        if R == 0:
+            return "Can't divide by zero"
+        else:
+            return UI**2/R
     elif UorI == 1:
-        """1 for using current I"""
+        #1 for using current I
         return R*I**2
     else:
+        #when the input for UorI isn't 0 or 1 -> return an error message
         return "Wrong value for the UorI input"

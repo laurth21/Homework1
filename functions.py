769ef786a8259f886202539a750015f5f542d010
laurth21 <e11779607@student.tuwien.ac.at>
2024-03-20 17:31:14 +0100

    add comments to if statements

--- functions.py
+++ functions.py
@@ -1,21 +1,21 @@
 def serialResistor(R1,R2):
     """Calculating the overall resistor value of two resistors R1 and R2 in serial by adding their values"""
-    if R1 < 0 or R2 < 0:
+    if R1 < 0 or R2 < 0: #making sure R1 or R2 aren't negative
         return "Negative value for resistor"
     else: return (R1+R2)
 
 def parallelResistor(R1,R2):
     """Calculating the overall resistor value of two resistors R1 and R2 in parallel by multiplying their values and dividing them by their sum"""
-    if R1 == 0 and R2 == 0:
+    if R1 == 0 and R2 == 0: #making sure there is no division by zero
         return "Can't divide by zero"
-    elif R1 < 0 or R2 < 0:
+    elif R1 < 0 or R2 < 0: #making sure R1 or R2 aren't negative
         return "Negative value for resistor"
     else:
         return (R1*R2/(R1+R2))
 
 def current(R,U):
     """Calculating the current throw a resistor by using the Ohm's law"""
-    if R <= 0:
+    if R <= 0: #making sure R isn't negative or zero (Division)
         return "Can't divide by zero or negative value for R"
     else:
         return U/R
@@ -23,12 +23,12 @@ def current(R,U):
 def DcPower(R,UI,UorI):
     """Calculating the power by a resistor using the voltage or current throw the resistor"""
     if UorI == 0: #0 for using voltage U
-        if R <= 0:
+        if R <= 0: #making sure R isn't negative or zero (Division)
             return "Can't divide by zero or negative value for R"
         else:
             return UI**2/R
     elif UorI == 1: #1 for using current I
-        if R < 0:
+        if R < 0: #making sure R isn't negative
             return "Negative value for R"
         else: return R*UI**2
     else:

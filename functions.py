26968766933dcef3a0c7ddc5a31cd31cbf3e8066
laurth21 <e11779607@student.tuwien.ac.at>
2024-03-20 16:53:15 +0100

    Update function file
    changed the if statements from R == 0 to R<=0 so no negativ resistor values are allowed.
    Added a elif in the parallelResistor function so R1 or R2 cant be negativ

--- functions.py
+++ functions.py
@@ -6,13 +6,15 @@ def parallelResistor(R1,R2):
     """Calculating the overall resistor value of two resistors R1 and R2 in parallel by multiplying their values and dividing them by their sum"""
     if R1 == 0 and R2 == 0:
         return "Can't divide by zero"
+    elif R1 < 0 or R2 < 0
+        return "Negative value for resistor"
     else:
         return (R1*R2/(R1+R2))
 
 def current(R,U):
     """Calculating the current throw a resistor by using the Ohm's law"""
-    if R == 0:
-        return "Can't divide by zero"
+    if R <= 0:
+        return "Can't divide by zero or negative value for R"
     else:
         return U/R
 
@@ -20,8 +22,8 @@ def DcPower(R,UI,UorI):
     """Calculating the power by a resistor using the voltage or current throw the resistor"""
     if UorI == 0:
         # 0 for using voltage U
-        if R == 0:
-            return "Can't divide by zero"
+        if R <= 0:
+            return "Can't divide by zero or negative value for R"
         else:
             return UI**2/R
     elif UorI == 1:
